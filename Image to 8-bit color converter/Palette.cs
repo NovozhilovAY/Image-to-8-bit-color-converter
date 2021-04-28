466ef028a93ebdac419142f40561926af8be0699
Alexandr <sasha.n191@yandex.ru>
2021-04-28 11:14:38 +0300

    add fill_colors method

--- Image to 8-bit color converter/Palette.cs
+++ Image to 8-bit color converter/Palette.cs
@@ -20,5 +20,20 @@ namespace Image_to_8_bit_color_converter
             return colors;
         }
 
+        protected void fill_colors()
+        {
+            for(int r = 0; r < R_vals.Length; r++)
+            {
+                for(int g = 0; g < G_vals.Length; g++)
+                {
+                    for (int b = 0; b < B_vals.Length; b++)
+                    {
+                        Color new_color = new Color();
+                        new_color = Color.FromArgb(R_vals[r], G_vals[g], B_vals[b]);
+                        colors.Add(new_color);
+                    }
+                }
+            }
+        }
     }
 }
