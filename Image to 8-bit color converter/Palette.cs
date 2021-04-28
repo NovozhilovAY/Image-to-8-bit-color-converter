5ed475d67cde1c6f98e09816f2db5c2b2ed1a766
Alexandr <sasha.n191@yandex.ru>
2021-04-28 11:03:14 +0300

    add Palette class

---
+++ Image to 8-bit color converter/Palette.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Image_to_8_bit_color_converter
+{
+    class Palette
+    {
+        private List<Color> colors;
+
+        protected int[] R_vals;
+        protected int[] G_vals;
+        protected int[] B_vals;
+
+        public List<Color> get_colors()
+        {
+            return colors;
+        }
+
+    }
+}
