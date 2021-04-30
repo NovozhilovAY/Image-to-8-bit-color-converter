609e17c5de0620d0a53e85672093f948f5dbb9da
Alexandr <sasha.n191@yandex.ru>
2021-04-30 16:16:39 +0300

    Fix convert method

--- Image to 8-bit color converter/ImageToPaletteConverter.cs
+++ Image to 8-bit color converter/ImageToPaletteConverter.cs
@@ -13,12 +13,12 @@ namespace Image_to_8_bit_color_converter
         public void convert(Bitmap image, Palette _palette)
         {
             colors = _palette.get_colors();
-            for(int i = 0; i < image.Height; i++)
+            for (int i = 0; i < image.Height; i++)
             {
-                for(int j = 0; j < image.Width; j++)
+                for (int j = 0; j < image.Width; j++)
                 {
-                    Color new_color = get_similar_color_from_palette(image.GetPixel(i, j));
-                    image.SetPixel(i, j, new_color);
+                    Color new_color = get_similar_color_from_palette(image.GetPixel(j, i));
+                    image.SetPixel(j, i, new_color);
                 }
             }
         }
@@ -31,10 +31,10 @@ namespace Image_to_8_bit_color_converter
             int B = color.B;
             int min_dif = int.MaxValue;
             int min_ind = 0;
-            for(int i = 0; i < colors.Count; i++)
+            for (int i = 0; i < colors.Count; i++)
             {
                 int dif = Math.Abs(colors[i].R - R) + Math.Abs(colors[i].G - G) + Math.Abs(colors[i].B - B);
-                if(dif < min_dif)
+                if (dif < min_dif)
                 {
                     min_dif = dif;
                     min_ind = i;
