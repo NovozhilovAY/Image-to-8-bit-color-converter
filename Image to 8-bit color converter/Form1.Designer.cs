e21630360071fe01070b376f82b100e924d20065
Alexandr <sasha.n191@yandex.ru>
2021-05-02 12:32:46 +0300

    Add trackBar

--- Image to 8-bit color converter/Form1.Designer.cs
+++ Image to 8-bit color converter/Form1.Designer.cs
@@ -29,21 +29,67 @@ namespace Image_to_8_bit_color_converter
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(808, 497);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 462);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Обработать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 411);
+            this.trackBar1.Maximum = 30;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(784, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(808, 497);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
 
--- Image to 8-bit color converter/Form1.cs
+++ Image to 8-bit color converter/Form1.cs
@@ -12,6 +12,14 @@ namespace Image_to_8_bit_color_converter
 {
     public partial class Form1 : Form
     {
+        const int MAX_PIXEL_SIZE = 30;
+        private Palette p8b;
+        private Palette p12b;
+        private Palette p16b;
+        private Pixelizer pixelizer;
+        private List<Task> tasks; 
+        private Bitmap[] images;
+        private Bitmap image;
         public Form1()
         {
             InitializeComponent();
@@ -19,9 +27,49 @@ namespace Image_to_8_bit_color_converter
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Palette8bit p = new Palette8bit();
-            Palette12bit p2 = new Palette12bit();
-            Palette16bit p3 = new Palette16bit();
+            image = (Bitmap)Image.FromFile("C:\\Users\\sasha\\source\\repos\\Image to 8-bit color converter\\test.jpg");
+            images = new Bitmap[MAX_PIXEL_SIZE];
+            images[0] = image;
+            tasks = new List<Task>();
+            pixelizer = new Pixelizer();
+            for (int i = 1; i < MAX_PIXEL_SIZE; i++)
+            {
+                int localI = i;
+                Bitmap tmp = new Bitmap(image);
+                Task t = Task.Run(() => pixelize_image(tmp, localI, localI + 1));
+                tasks.Add(t);
+            }
+            Task.WaitAll(tasks.ToArray());
+            p8b = new Palette8bit();
+            p12b = new Palette12bit();
+            p16b = new Palette16bit();
+            pictureBox1.Image = image;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ImageToPaletteConverter converter = new ImageToPaletteConverter();
+            
+            //pictureBox1.Image = image;
+            //pictureBox1.Refresh();
+            Pixelizer p = new Pixelizer();
+            
+            Bitmap new_b = p.process(image,5);
+            converter.convert(new_b, p8b);
+            
+            pictureBox1.Image = new_b;
+            pictureBox1.Refresh();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            pictureBox1.Image = images[trackBar1.Value-1];
+
+        }
+
+        public void pixelize_image(Bitmap orig,int pos, int pixel_size)
+        {
+           images[pos] = pixelizer.process(orig, pixel_size); 
         }
     }
 }
