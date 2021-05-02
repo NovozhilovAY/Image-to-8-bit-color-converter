d522987a004e113d7647f9aa3c673f3746ccd8c9
Alexandr <sasha.n191@yandex.ru>
2021-05-02 14:16:30 +0300

    Add tasks in ImageToPaletteConverter

--- Image to 8-bit color converter/ImageToPaletteConverter.cs
+++ Image to 8-bit color converter/ImageToPaletteConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,21 +10,60 @@ namespace Image_to_8_bit_color_converter
 {
     class ImageToPaletteConverter
     {
+        private List<List<Color>> rows_of_colors;
         private List<Color> colors;
+        private List<Task> tasks;
         public void convert(Bitmap image, Palette _palette)
         {
             colors = _palette.get_colors();
+
+            //for (int i = 0; i < image.Height; i++)
+            //{
+            //    for (int j = 0; j < image.Width; j++)
+            //    {
+            //        Color new_color = get_similar_color_from_palette(image.GetPixel(j, i));
+            //        image.SetPixel(j, i, new_color);
+            //    }
+            //}
             for (int i = 0; i < image.Height; i++)
             {
+                rows_of_colors[i] = new List<Color>();
                 for (int j = 0; j < image.Width; j++)
                 {
-                    Color new_color = get_similar_color_from_palette(image.GetPixel(j, i));
-                    image.SetPixel(j, i, new_color);
+                    rows_of_colors[i].Add(image.GetPixel(j, i));
+                }
+            }
+
+            for(int i = 0; i < rows_of_colors.Count;i++)
+            {
+                int cur_i = i;
+                Task t = new Task(() => Process_row(rows_of_colors[cur_i]));
+                tasks.Add(t);
+            }
+            Task.WaitAll(tasks.ToArray());
+            Set_new_colors(image);
+        }
+
+        void Process_row(List<Color> row)
+        {
+            for (int i = 0; i < row.Count; i++)
+            {
+                row[i] = Get_similar_color_from_palette(row[i]);
+            }
+        }
+
+        private void Set_new_colors(Bitmap image)
+        {
+            for(int y = 0;y < image.Height;y++)
+            {
+                for(int x = 0;x<image.Width;x++)
+                {
+                    image.SetPixel(x, y, rows_of_colors[y][x]);
                 }
             }
         }
 
-        private Color get_similar_color_from_palette(Color color)
+        private Color Get_similar_color_from_palette(Color color)
         {
             Color new_color;
             int R = color.R;
